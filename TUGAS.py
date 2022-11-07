3f3a75e00451e51e7f9825de936ec9233ceeee69
Fadhlan <fadhlandeveloper@gmail.com>
2022-11-07 09:48:05 +0700

    TugasPython1

---
+++ TUGAS.py
@@ -0,0 +1,22 @@
+##Soal 1 Nih NGAB
+print("Hello World Python"),
+
+##Soal 2 Nih NGAB
+TKJ = 10
+RPL = 20
+
+print(TKJ + RPL)
+print(RPL - TKJ)
+print(TKJ * RPL)
+print( int(RPL / TKJ)),
+
+##Soal 2 Nih NGAB
+print( int(RPL / TKJ))
+print( float(RPL / TKJ)),
+
+
+##Soal 4 Nih NGAB
+Age = 9999
+Answ = "My Age :"
+
+print(Answ, Age)
